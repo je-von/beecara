61d1304e7cb7d864507b58332f6e2f9f93e2ac41
faustinaodetaa <faustinaodeta@gmail.com>
2022-10-24 14:40:09 +0700

    add description

--- pages/index.tsx
+++ pages/index.tsx
@@ -73,7 +73,7 @@ const Home: NextPage = () => {
             <div className="flex flex-col w-full md:w-2/5 justify-center items-start text-center md:text-left">
               <p className="uppercase tracking-loose w-full">Welcome to</p>
               <h1 className="my-4 text-5xl font-bold leading-tight">BeeCara</h1>
-              <p className="leading-normal text-2xl mb-8">An application to blablabla bla. Bla bla bla bla, bla!</p>
+              <p className="leading-normal text-2xl mb-8">An application for Binusian to seek and join events to get SAT points and Comserv Hour!</p>
               <button
                 onClick={loginWithOutlook}
                 className="flex items-center justify-center mx-auto lg:mx-0 hover:underline bg-white text-gray-800 font-bold rounded-full my-6 py-4 px-8 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out"
