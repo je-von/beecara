14f4d992d0accda73d06b504a4de9169f05427cd
Kezia Gloria <keziagloria24@gmail.com>
2022-10-28 20:38:45 +0700

    detail page

--- components/card.tsx
+++ components/card.tsx
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import Link from 'next/link'
 import React, { ReactNode, useEffect, useState } from 'react'
-import {FaCalendar} from "react-icons/fa";
+import {FaCalendar, FaMonument} from "react-icons/fa";
 import {BsPeopleFill} from "react-icons/bs";
 import {GiAchievement} from "react-icons/gi";
 import { collection, onSnapshot, query} from 'firebase/firestore';
@@ -22,48 +22,53 @@ interface Event {
 }
 
 const Card=({ eventID, image, name, organization, capacity, maxCapacity, eventDate, benefit}: Event)=>{
-  // const [events, setEvents] = useState(initialState);
+  const [events, setEvents] = useState([]);
   useEffect(() => {
     const collectionRef = collection(db, "event")
-    console.log('hi')
     const q = query(collectionRef);
     const test = onSnapshot(q, (querySnapshot) =>{
-      querySnapshot.docs.map(doc => (
-        console.log(doc.data())
-      ))
+      setEvents(querySnapshot.docs.map(doc => (
+        // console.log(doc.data() + ' ' + doc.id)
+        {...doc.data(), id: doc.id}
+        )))
     })
-    
+    return test
   }, [])
+  
   
 
   return(
-    <Link href={`event/${eventID}`} key={eventID}>
+    <div>
+      {events.map(event=>
+    <Link href={`event/${event.id}`} key={event.id}>
       <div className='rounded-lg p-5 bg-blue-100 flex justify-between items-start mt-4'>
         <div className='flex items-center'>
           <div className='border border-gray-400 rounded-full mr-5 overflow-hidden'>
-            <Image className='rounded-full' objectFit='contain' src={image} alt="event-img" width={50} height={50}>
+            <Image className='rounded-full' objectFit='contain' src={event.image} alt="event-img" width={50} height={50}>
             </Image>
           </div>
           <div className='flex items-center '>
 
             <div className='mr-8'>
-              <h3> <b>{name}</b> - {organization}</h3>
+              <h3> <b>{event.name}</b> - {event.organization}</h3>
               <p>
                 <div className='flex items-center'>
                   <FaCalendar className='mr-1'/>
-                  {eventDate}
+                  {event.startDate.toDate().toDateString()}
                 </div>
               </p>
               <p>
                 <div className='flex items-center'>
                   <BsPeopleFill className='mr-1'/>
-                  {capacity} / {maxCapacity}
+                  {capacity} / {event.capacity}
                 </div>
               </p>
               <p>
                 <div className='flex items-center'>
                   <GiAchievement className='mr-1'/>
-                  {benefit}
+                  { event.benefit.map(b => 
+                    b.type  + ' : ' + b.amount + ' '
+                  )}
                 </div>
               </p>
             </div>
@@ -80,6 +85,8 @@ const Card=({ eventID, image, name, organization, capacity, maxCapacity, eventDa
       </div>
       
     </Link>
+        )}
+    </div>
   )
 }
 
--- pages/event/[slug].tsx
+++ pages/event/[slug].tsx
@@ -1,10 +1,13 @@
 import type { NextPage } from "next";
-import React, { ReactNode } from 'react'
+import React, { ReactNode, useEffect, useState } from 'react'
 import {FaCalendar, FaClock} from "react-icons/fa";
 import {BsPeopleFill} from "react-icons/bs";
 import {GiAchievement} from "react-icons/gi";
 import {IoMdArrowBack} from "react-icons/io";
 import Image from 'next/image'
+import { useRouter } from "next/router";
+import { collection, doc, getDoc, getDocFromCache } from "firebase/firestore";
+import { db } from "../../lib/firebaseConfig/init";
 
 interface Event {
   eventID: string 
@@ -22,41 +25,56 @@ interface Event {
 }
 
 const EventDetail = ({name, description, organization, capacity, maxCapacity, eventDate, startTime, endTime, benefit} : Event) => {
-
-
+  const router = useRouter()
+  var eventID = router.query["slug"]?.toString()
+  const [event, setEvent] = useState([]);
+  useEffect(() => {
+   async function fetchData() {
+     const docRef = doc(db, "event", eventID)
+     const docSnap = await getDoc(docRef);
+     if(docSnap.exists()){
+       console.log(docSnap.data())
+       setEvent(docSnap.data())
+     }
+     console.log('hi')
+   } 
+   fetchData()
+  },[])
   return(
     <div className="px-40">
       <div className="flex items-center">
         <IoMdArrowBack className="mr-2 text-xl cursor-pointer"/>
-        <h1 className="font-bold text-xl">{name} Event 1 - HIMSISFO {organization}</h1>
+        <h1 className="font-bold text-xl">{event.name} - {event.organization}</h1>
       </div>
       <div className='overflow-hidden'>
-        <Image className='rounded-xl' objectFit='contain' src={'/assets/home_vector.png'} alt="event-poster" width={150} height={150}>
+        <Image className='rounded-xl' objectFit='contain' src={event.image} alt="event-poster" width={150} height={150}>
               </Image>
       </div>
-      <p className="text-justify">{description} Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestias, inventore neque nihil recusandae aspernatur quidem impedit, necessitatibus deserunt accusamus nostrum provident modi corrupti? Ab doloribus culpa quidem quia quo perferendis!</p>
+      <p className="text-justify">{event.description} </p>
       <p>
         <div className="flex items-center">
           <FaCalendar className="mr-1"/>
-          {eventDate} 27 Oct 2022
+          {event.startDate?.toDate().toDateString()}
         </div>
       </p>
       <p>
         <div className="flex items-center">
           <FaClock className="mr-1"/>
-          {startTime} 09:00 - 13:00{endTime}
+          {event.startDate?.toDate().toLocaleTimeString('en-US')} - {event.endDate?.toDate().toLocaleTimeString('en-US')}
         </div>
       </p>
       <p>
         <div className="flex items-center">
           <GiAchievement className="mr-1"/>
-          {benefit} 5 SAT
+          { event.benefit.map(b => 
+            b.type  + ' : ' + b.amount + ' '
+          )}
         </div>
       </p>
       <p>
         <div className="flex items-center">
           <BsPeopleFill className="mr-1"/>
-          {capacity} 2 / 100{maxCapacity}
+          {capacity} 2 / {event.capacity}
         </div>
       </p>
       <div className="border border-blue-500 rounded-lg bg-blue-500 text-white px-3 cursor-pointer w-fit items-center mt-2">
