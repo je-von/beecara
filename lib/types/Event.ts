2ce1ae7372cbb9c200e94005ab7d2e939f956eb8
Jevon Levin <jevon.levin@gmail.com>
2022-10-31 21:08:59 +0700

    fix: assign snapshot's ID as event's ID

--- lib/types/Event.ts
+++ lib/types/Event.ts
@@ -1,36 +1,28 @@
-import {
-  DocumentData,
-  Firestore,
-  QueryDocumentSnapshot,
-  SnapshotOptions,
-  Timestamp,
-  WithFieldValue,
-} from "firebase/firestore";
+import { DocumentData, Firestore, QueryDocumentSnapshot, SnapshotOptions, Timestamp, WithFieldValue } from 'firebase/firestore'
 
 class Benefit {
-  constructor(readonly amount: number = 0, readonly type: string = "") {}
+  constructor(readonly amount: number = 0, readonly type: string = '') {}
 }
 
 class Event {
   constructor(
-    readonly eventId: string = "",
-    readonly image: string = "",
-    readonly name: string = "",
-    readonly organization: string = "",
+    readonly eventId: string = '',
+    readonly image: string = '',
+    readonly name: string = '',
+    readonly organization: string = '',
     readonly capacity: number = 0,
     readonly maxCapacity: number = 0,
     readonly benefit?: Benefit[],
     readonly startDate?: Timestamp,
     readonly endDate?: Timestamp,
-    readonly description: string = ""
-  ) {
-  }
+    readonly description: string = ''
+  ) {}
 }
 
 const eventConverter = {
   toFirestore(event: WithFieldValue<Event>): DocumentData {
     return {
-      eventId: event.eventId,
+      eventId: event.eventId, // TODO: fix (kayaknya id nya gausah disimpen di fields lagi?)
       image: event.image,
       name: event.name,
       organization: event.organization,
@@ -40,25 +32,12 @@ const eventConverter = {
       startDate: event.startDate,
       endDate: event.endDate,
       description: event.description,
-    };
+    }
   },
-  fromFirestore(
-    snapshot: QueryDocumentSnapshot,
-    options: SnapshotOptions
-  ): Event {
-    const data = snapshot.data(options)!;
-    return new Event(
-      data.eventId,
-      data.image,
-      data.name,
-      data.organization,
-      data.capacity,
-      data.maxCapacity,
-      data.benefit,
-      data.startDate,
-      data.endDate,
-      data.description,);
+  fromFirestore(snapshot: QueryDocumentSnapshot, options: SnapshotOptions): Event {
+    const data = snapshot.data(options)!
+    return new Event(snapshot.id, data.image, data.name, data.organization, data.capacity, data.maxCapacity, data.benefit, data.startDate, data.endDate, data.description)
   },
-};
+}
 
-export { Event, eventConverter, Benefit };
+export { Event, eventConverter, Benefit }
