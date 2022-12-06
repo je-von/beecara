234c682e358fbf462906bf8bd241b17f170b018d
Winston Khoe <winstonkhoe@yahoo.com>
2022-12-06 23:20:00 +0700

    feat(events): add register status for event

--- components/event/card.tsx
+++ components/event/card.tsx
@@ -5,22 +5,29 @@ import { FaCalendar } from 'react-icons/fa'
 import { BsPeopleFill } from 'react-icons/bs'
 import { Event } from '../../lib/types/Event'
 import { organizationConverter } from '../../lib/types/Organization'
-import { useDocumentData } from 'react-firebase-hooks/firestore'
+import { useCollectionData, useDocumentData } from 'react-firebase-hooks/firestore'
+import { useAuth } from '../../lib/authContext'
+import { collection, limit, query, where } from 'firebase/firestore'
+import { db } from '../../lib/firebaseConfig/init'
+import { userConverter } from '../../lib/types/User'
 
 interface Props {
   event: Event
 }
 
 const Card = ({ event }: Props) => {
-  const ref = event.organization.withConverter(organizationConverter)
-  const [organization, loading, error] = useDocumentData(ref)
-
+  const { user: userAuth, loading: loadingAuth } = useAuth()
+  const organizationRef = event.organization.withConverter(organizationConverter)
+  const userRef = collection(db, 'user').withConverter(userConverter)
+  const [user, loadingUser, errorUser] = useCollectionData(query(userRef, where('email', '==', userAuth?.email), limit(1)))
+  const [organization, loadingOrganization, errorOrganization] = useDocumentData(organizationRef)
   //TODO: add spinner / skeleton
-  if (loading) return <h1>Loading...</h1>
-
+  if (loadingAuth || loadingUser || loadingOrganization) return <h1>Loading...</h1>
+  const isRegistered = user && user.length > 0 && event && event.users && event.users.length > 0 && event.users.some(u => u.id === user[0].userId)
   return (
     <Link href={`event/${event.eventId}`} key={event.eventId} passHref>
-      <div className="cursor-pointer transition-colors duration-500 rounded-lg border border-blue-100 hover:bg-blue-50 flex justify-between gap-5 items-start relative md:h-56 p-5 md:flex-row flex-col">
+      <div className={`cursor-pointer transition-colors duration-500 rounded-lg border border-blue-100 hover:bg-blue-50 flex justify-between gap-5 items-start relative md:h-56 p-5 md:flex-row flex-col`}>
+        { isRegistered && <div className="absolute bottom-6 right-6 text-sm font-bold text-white bg-blue-400 px-4 py-2 rounded-lg">Registered</div> }
         <div className="md:basis-1/6 lg:w-auto w-full h-40 md:w-20 md:h-full relative rounded">
           <Image className="relative rounded" objectFit="cover" src={event.image} alt={event.name} sizes="100%" layout="fill"></Image>
         </div>
