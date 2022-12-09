7861db0c72b1e643bf1d25a1348c55af5fb057d6
Jevon Levin <jevon.levin@gmail.com>
2022-12-09 19:48:54 +0700

    fix: form input (subscribe getFieldState to formState so it becomes reactive to error state update)

--- components/form/FormInput.tsx
+++ components/form/FormInput.tsx
@@ -15,7 +15,7 @@ interface Props {
 }
 
 const Input = ({ name, validation, inputType, placeholder, titleLabel, title, width, isDisabled, value, additionalAppend, additionalPrepend }: Props) => {
-  const { register, getFieldState } = useFormContext()
+  const { register, getFieldState, formState } = useFormContext()
   const RawInput = (props: any) => {
     if (props.type === 'textarea') {
       return <textarea {...props} {...register(name, validation)} className={'h-32 ' + props.className}></textarea>
@@ -23,6 +23,7 @@ const Input = ({ name, validation, inputType, placeholder, titleLabel, title, wi
       return <input {...register(name, validation)} {...props} min={validation?.min} />
     }
   }
+
   return (
     <div className={`${width === 'full' ? 'md:w-full' : width === '1/2' ? 'md:w-1/2' : 'md:w-1/3'} w-full px-3 mb-5 flex flex-col gap-2 `}>
       <label className="flex gap-2 uppercase tracking-wide text-gray-700 text-xs font-bold w-full">
@@ -32,7 +33,7 @@ const Input = ({ name, validation, inputType, placeholder, titleLabel, title, wi
         {additionalPrepend}
         <RawInput
           className={`resize-none appearance-none block w-full ${isDisabled ? 'bg-gray-200' : 'bg-white'} text-gray-700 border ${
-            getFieldState(name).error ? 'border-red-500' : 'border-gray-300'
+            getFieldState(name, formState).error ? 'border-red-500' : 'border-gray-300'
           } rounded py-3 px-4 leading-tight focus:outline-none  focus:ring-sky-400 focus:bg-white ${additionalAppend ? 'rounded-r-none' : ''} ${
             additionalPrepend ? 'rounded-l-none' : ''
           } ${isDisabled ? 'cursor-not-allowed' : ''}`}
@@ -43,7 +44,9 @@ const Input = ({ name, validation, inputType, placeholder, titleLabel, title, wi
         />
         {additionalAppend}
       </div>
-      {getFieldState(name).error && <p className="text-red-500 text-xs italic">{getFieldState(name).error?.message || `${title ? title : titleLabel} is invalid`}</p>}
+      {getFieldState(name, formState).error && (
+        <p className="text-red-500 text-xs italic">{getFieldState(name, formState).error?.message || `${title ? title : titleLabel} is invalid`}</p>
+      )}
     </div>
   )
 }
