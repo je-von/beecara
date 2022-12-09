172887381e3d1a203d0e9f1f7847f46466d7cd43
Winston Khoe <winstonkhoe@yahoo.com>
2022-12-09 19:51:29 +0700

    fix(card): fix event slot

--- components/event/card.tsx
+++ components/event/card.tsx
@@ -1,36 +1,38 @@
-import Image from 'next/image'
-import Link from 'next/link'
-import { FaCalendar } from 'react-icons/fa'
-import { BsPeopleFill } from 'react-icons/bs'
-import { Event, eventRegisteredUsersConverter } from '../../lib/types/Event'
-import { useCollectionData } from 'react-firebase-hooks/firestore'
-import { useAuth } from '../../lib/authContext'
-import { collection, doc, query, where } from 'firebase/firestore'
-import { db } from '../../lib/firebaseConfig/init'
-import Skeleton from 'react-loading-skeleton'
-import BenefitTags from './BenefitTags'
+import Image from "next/image";
+import Link from "next/link";
+import { FaCalendar } from "react-icons/fa";
+import { BsPeopleFill } from "react-icons/bs";
+import { Event, eventRegisteredUsersConverter } from "../../lib/types/Event";
+import { useCollectionData } from "react-firebase-hooks/firestore";
+import { useAuth } from "../../lib/authContext";
+import { collection, doc, query, where } from "firebase/firestore";
+import { db } from "../../lib/firebaseConfig/init";
+import Skeleton from "react-loading-skeleton";
+import BenefitTags from "./BenefitTags";
 
 interface Props {
-  event: Event
-  showRegisterStatus?: Boolean
-  showImage?: Boolean
-  showTitle?: Boolean
-  showDate?: Boolean
-  showSlot?: Boolean
-  showOrganizer?: Boolean
-  showBenefits?: Boolean
-  horizontalLayout?: Boolean
+  event: Event;
+  showRegisterStatus?: Boolean;
+  showImage?: Boolean;
+  showTitle?: Boolean;
+  showDate?: Boolean;
+  showSlot?: Boolean;
+  showOrganizer?: Boolean;
+  showBenefits?: Boolean;
+  horizontalLayout?: Boolean;
 }
 
 export const SkeletonCard = () => (
-  <div className={`p-5 h-full w-full rounded-lg shadow-lg bg-white flex flex-col justify-between`}>
+  <div
+    className={`p-5 h-full w-full rounded-lg shadow-lg bg-white flex flex-col justify-between`}
+  >
     <div className="flex flex-col gap-5">
-      <Skeleton height={120} width={'100%'} />
-      <Skeleton count={3} width={'100%'} height={25} />
+      <Skeleton height={120} width={"100%"} />
+      <Skeleton count={3} width={"100%"} height={25} />
     </div>
-    <Skeleton count={2} width={'100%'} height={25} />
+    <Skeleton count={2} width={"100%"} height={25} />
   </div>
-)
+);
 
 const Card = ({
   event,
@@ -43,47 +45,70 @@ const Card = ({
   showBenefits = true,
   horizontalLayout,
 }: Props) => {
-  const { user, loading: loadingAuth } = useAuth()
-  const ref = collection(db, `event/${event.eventId}/registeredUsers`).withConverter(eventRegisteredUsersConverter)
-  const [data, loadingRegistered, error] = useCollectionData(query(ref, where('user', '==', doc(db, 'user', `${user?.userId}`))))
-
-  if (loadingAuth || loadingRegistered) return <SkeletonCard />
+  const { user, loading: loadingAuth } = useAuth();
+  const ref = collection(
+    db,
+    `event/${event.eventId}/registeredUsers`
+  ).withConverter(eventRegisteredUsersConverter);
+  const [data, loadingRegistered, error] = useCollectionData(
+    query(ref, where("user", "==", doc(db, "user", `${user?.userId}`)))
+  );
+  if (loadingAuth || loadingRegistered) return <SkeletonCard />;
   // const isRegistered = event?.users?.some((u) => u.id === user?.userId)
-  const isRegistered = data?.length && data[0].status
+  const isRegistered = data?.length && data[0].status;
   return (
     <Link href={`event/${event.eventId}`} key={event.eventId} passHref>
       <div
         className={`border cursor-pointer transition-all duration-[400ms] overflow-hidden rounded-lg shadow-lg hover:ring-2 hover:ring-sky-300 bg-white flex justify-between gap-5 items-start relative p-2 sm:p-3 md:p-5 ${
-          !horizontalLayout ? 'flex-col' : ''
+          !horizontalLayout ? "flex-col" : ""
         }`}
       >
         {showRegisterStatus && isRegistered ? (
           <div
             className={`absolute top-0 left-0 z-10 text-xs sm:text-sm font-bold text-white ${
-              data[0].status === 'Registered' ? 'bg-sky-400' : data[0].status === 'Pending' ? 'bg-orange-400' : 'bg-red-400'
+              data[0].status === "Registered"
+                ? "bg-sky-400"
+                : data[0].status === "Pending"
+                ? "bg-orange-400"
+                : "bg-red-400"
             } px-4 py-2 rounded-br-xl`}
           >
             {data[0].status}
           </div>
         ) : undefined}
         {showImage && (
-          <div className={`w-full h-40 ${horizontalLayout ? 'basis-1/3' : ''} relative rounded`}>
-            <Image className="relative rounded" objectFit="cover" src={event.image} alt={event.name} sizes="100%" layout="fill"></Image>
+          <div
+            className={`w-full h-40 ${
+              horizontalLayout ? "basis-1/3" : ""
+            } relative rounded`}
+          >
+            <Image
+              className="relative rounded"
+              objectFit="cover"
+              src={event.image}
+              alt={event.name}
+              sizes="100%"
+              layout="fill"
+            ></Image>
           </div>
         )}
         <div className="flex flex-1 items-start">
           <div className="flex flex-col gap-1 h-full justify-between">
             <div className="flex flex-col gap-1">
-              {showTitle && <h4 className="font-secondary text-md md:text-xl mb-1 gap-2 flex flex-col ">{event.name}</h4>}
+              {showTitle && (
+                <h4 className="font-secondary text-md md:text-xl mb-1 gap-2 flex flex-col ">
+                  {event.name}
+                </h4>
+              )}
               <div className="flex flex-col md:gap-6 lg:gap-1">
                 {showDate && (
                   <div className="flex items-center gap-1">
                     <FaCalendar className="text-gray-400" />
-                    {event.startDate?.toDate().toLocaleDateString('en-US', {
-                      weekday: 'short',
-                      year: 'numeric',
-                      month: 'short',
-                      day: 'numeric',
+                    {event.startDate?.toDate().toLocaleDateString("en-US", {
+                      weekday: "short",
+                      year: "numeric",
+                      month: "short",
+                      day: "numeric",
                     })}
                   </div>
                 )}
@@ -91,7 +116,7 @@ const Card = ({
                 {showSlot && (
                   <div className="flex items-center gap-1">
                     <BsPeopleFill className="text-gray-400" />
-                    {/* {event.users?.length} / {event.capacity} */}
+                    {data ? data.length : 0} / {event.capacity}
                   </div>
                 )}
               </div>
@@ -104,7 +129,9 @@ const Card = ({
               )}
               {showOrganizer && (
                 <div className="flex justify-start">
-                  <h5 className="text-xs sm:text-sm font-bold">{event.organization.id}</h5>
+                  <h5 className="text-xs sm:text-sm font-bold">
+                    {event.organization.id}
+                  </h5>
                 </div>
               )}
             </div>
@@ -112,7 +139,7 @@ const Card = ({
         </div>
       </div>
     </Link>
-  )
-}
+  );
+};
 
-export default Card
+export default Card;
--- components/event/events.tsx
+++ components/event/events.tsx
@@ -1,37 +1,61 @@
-import { collection, orderBy, query } from 'firebase/firestore'
-import { useEffect, useState } from 'react'
-import { useCollectionData } from 'react-firebase-hooks/firestore'
-import { BsCalendar, BsCheckSquareFill, BsFilter, BsSquare, BsViewStacked } from 'react-icons/bs'
-import { db } from '../../lib/firebaseConfig/init'
-import { eventConverter } from '../../lib/types/Event'
-import UnderlineButton from '../button/UnderlineButton'
-import CalendarEventView from './calendar'
-import Card, { SkeletonCard } from './card'
+import { collection, orderBy, query } from "firebase/firestore";
+import { useEffect, useRef, useState } from "react";
+import { useCollectionData } from "react-firebase-hooks/firestore";
+import {
+  BsCalendar,
+  BsCheckSquareFill,
+  BsFilter,
+  BsSquare,
+  BsViewStacked,
+} from "react-icons/bs";
+import { db } from "../../lib/firebaseConfig/init";
+import { eventConverter } from "../../lib/types/Event";
+import UnderlineButton from "../button/UnderlineButton";
+import CalendarEventView from "./calendar";
+import Card, { SkeletonCard } from "./card";
 
 const EventList = () => {
-  const [showFilterDropdown, setShowFilterDropdown] = useState<Boolean>(false)
-  const [isCalendarView, setIsCalendarView] = useState<Boolean>(false)
-  const [benefitFilters, setFilters] = useState<string[]>([])
-  const [keyword, setKeyword] = useState('')
-  const ref = collection(db, 'event').withConverter(eventConverter)
-  const [data, loading, error] = useCollectionData(query(ref, orderBy('startDate', 'asc')))
+  const [showFilterDropdown, setShowFilterDropdown] = useState<Boolean>(false);
+  const [isCalendarView, setIsCalendarView] = useState<Boolean>(false);
+  const [calendarStyle, setCalendarStyle] = useState('')
+  const [benefitFilters, setFilters] = useState<string[]>([]);
+  const [keyword, setKeyword] = useState("");
+  const listHeight = useRef(null);
+  const ref = collection(db, "event").withConverter(eventConverter);
+  const [data, loading, error] = useCollectionData(
+    query(ref, orderBy("startDate", "asc"))
+  );
   useEffect(() => {
-    console.log(benefitFilters)
-  }, [benefitFilters])
+    console.log(benefitFilters);
+  }, [benefitFilters]);
 
-  const toggleDropdown = () => setShowFilterDropdown((s) => !s)
+  useEffect(() => {
+    if (isCalendarView === false) {
+      setTimeout(() => {
+        setCalendarStyle('hidden')
+      }, 300)
+    } else setCalendarStyle('')
+  }, [isCalendarView])
+
+  const toggleDropdown = () => setShowFilterDropdown((s) => !s);
 
   return (
     <div className="py-5 flex flex-col gap-4">
       <div>
         <h1 className="text-2xl font-black font-secondary">Events</h1>
-        <p>Blablabla, kasih text gitu kek: register to any of the following events to increase your SAT ato apa gt!</p>
+        <p>
+          Blablabla, kasih text gitu kek: register to any of the following
+          events to increase your SAT ato apa gt!
+        </p>
       </div>
       <div className="flex flex-col gap-4">
         <div className="flex md:flex-row flex-col gap-2 justify-between items-center">
           <div className="relative">
             <div className="flex items-center gap-7 text-sm my-2 relative">
-              <UnderlineButton className="flex gap-2 items-center cursor-pointer" onClick={() => setIsCalendarView(!isCalendarView)}>
+              <UnderlineButton
+                className="flex gap-2 items-center cursor-pointer"
+                onClick={() => setIsCalendarView(!isCalendarView)}
+              >
                 {isCalendarView ? (
                   <>
                     <BsViewStacked />
@@ -46,22 +70,42 @@ const EventList = () => {
               </UnderlineButton>
               {!isCalendarView && (
                 <div className="relative">
-                  <UnderlineButton className="flex gap-2 items-center cursor-pointer" onClick={toggleDropdown}>
+                  <UnderlineButton
+                    className="flex gap-2 items-center cursor-pointer"
+                    onClick={toggleDropdown}
+                  >
                     <BsFilter /> Filter
                   </UnderlineButton>
-                  <div className={`${!showFilterDropdown && 'hidden'} fixed z-20 top-0 left-0 w-[100vw] h-[100vh]`} onClick={toggleDropdown}></div>
-                  <div className={`${!showFilterDropdown && 'hidden'} absolute z-50 w-52 origin-top-right rounded-md border border-gray-100 bg-white shadow-lg`}>
+                  <div
+                    className={`${
+                      !showFilterDropdown && "hidden"
+                    } fixed z-20 top-0 left-0 w-[100vw] h-[100vh]`}
+                    onClick={toggleDropdown}
+                  ></div>
+                  <div
+                    className={`${
+                      !showFilterDropdown && "hidden"
+                    } absolute z-50 w-52 origin-top-right rounded-md border border-gray-100 bg-white shadow-lg`}
+                  >
                     <div className="p-2">
-                      {['SAT Points', 'ComServ Hours'].map((e, index) => (
+                      {["SAT Points", "ComServ Hours"].map((e, index) => (
                         <div
                           key={index}
                           className="rounded-lg px-4 py-2 text-sm text-gray-500 hover:bg-gray-100 hover:text-gray-700 cursor-pointer flex items-center justify-between"
                           onClick={() => {
-                            setFilters((filters) => (filters.includes(e) ? filters.filter((f) => f != e) : filters.concat(e)))
+                            setFilters((filters) =>
+                              filters.includes(e)
+                                ? filters.filter((f) => f != e)
+                                : filters.concat(e)
+                            );
                           }}
                         >
                           {e}
-                          {benefitFilters.includes(e) ? <BsCheckSquareFill className="text-sky-400" /> : <BsSquare className="text-gray-400" />}
+                          {benefitFilters.includes(e) ? (
+                            <BsCheckSquareFill className="text-sky-400" />
+                          ) : (
+                            <BsSquare className="text-gray-400" />
+                          )}
                         </div>
                       ))}
                     </div>
@@ -81,22 +125,44 @@ const EventList = () => {
           )}
         </div>
       </div>
-      {isCalendarView ? (
-        <CalendarEventView events={data} />
-      ) : (
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 justify-center items-stretch gap-8">
+      {/* <div className="-translate-y-[120%]"></div> */}
+      <div className="overflow-hidden pb-4">
+        <div
+          ref={listHeight}
+          className={`transition-all grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 justify-center items-stretch gap-8`}
+          style={{
+            marginTop: isCalendarView
+              ? `-${listHeight.current.clientHeight + 20}px`
+              : "0",
+          }}
+        >
           {loading ? (
             <SkeletonCard />
           ) : (
             data
-              ?.filter((d) => d.benefit && benefitFilters.filter((bf) => d.benefit?.find((b) => b.type == bf)).length == benefitFilters.length)
-              .filter((d) => d.name.toLowerCase().includes(keyword) || d.organization.id.toLowerCase().includes(keyword))
+              ?.filter(
+                (d) =>
+                  d.benefit &&
+                  benefitFilters.filter((bf) =>
+                    d.benefit?.find((b) => b.type == bf)
+                  ).length == benefitFilters.length
+              )
+              .filter(
+                (d) =>
+                  d.name.toLowerCase().includes(keyword) ||
+                  d.organization.id.toLowerCase().includes(keyword)
+              )
               .map((d) => <Card key={d.eventId} event={d} />)
           )}
         </div>
-      )}
+        <div
+          className={`transition-all mt-8 ${calendarStyle}`}
+        >
+          <CalendarEventView events={data} />
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default EventList
+export default EventList;
