79ef83169e0a49bff87523a642cee15e1eefc4c4
Jevon Levin <jevon.levin@gmail.com>
2022-12-29 13:52:36 +0700

    feat: add presence column

--- components/event/RegistrantTable.tsx
+++ components/event/RegistrantTable.tsx
@@ -14,6 +14,11 @@ const RegistrantTable = ({ event }: Props) => {
       console.log('participant ' + status) // TODO: create alert / toast
     })
   }
+  const togglePresence = (userId: string, isPresent: boolean) => {
+    updateDoc(doc(db, 'event', `${event?.eventId}/registeredUsers/${userId}`), {
+      isPresent: isPresent
+    })
+  }
   return (
     <div className="overflow-x-auto relative shadow-lg">
       <table className="w-full text-sm text-left text-gray-400 ">
@@ -25,8 +30,11 @@ const RegistrantTable = ({ event }: Props) => {
             <th scope="col" className="py-3 px-6">
               Proof
             </th>
-            <th scope="col" className="py-3 px-6">
+            <th scope="col" className="py-3 px-6 text-center">
               Action
+            </th>
+            <th scope="col" className="py-3 px-6 text-center">
+              Present
             </th>
           </tr>
         </thead>
@@ -40,7 +48,7 @@ const RegistrantTable = ({ event }: Props) => {
             ?.map((ru) => (
               <tr className="bg-white border-b hover:bg-gray-50 " key={ru.userId}>
                 <th scope="row" className="py-4 px-6 font-medium text-gray-900 w-60">
-                  <div className="w-60">
+                  <div className={`w-60 ${ru.status === 'Rejected' ? 'line-through text-gray-400' : ''}`}>
                     {ru.user?.name} <div className="text-gray-400 truncate ">{ru.user?.email}</div>
                   </div>
                 </th>
@@ -58,9 +66,9 @@ const RegistrantTable = ({ event }: Props) => {
                   </div>
                 </td>
 
-                <td className="py-4 px-6 w-[20%]">
+                <td className="py-4 px-6 w-[25%] text-center">
                   {ru.status !== 'Rejected' && ru.status !== 'Registered' ? (
-                    <div className="flex gap-4 w-full">
+                    <div className="flex gap-4 w-full justify-center">
                       <Button
                         onClick={() => {
                           toggleParticipant(ru?.userId!, 'Registered')
@@ -78,8 +86,16 @@ const RegistrantTable = ({ event }: Props) => {
                       </Button>
                     </div>
                   ) : (
-                    <div className="">{ru.status}</div>
+                    <div className={ru.status === 'Registered' ? 'text-sky-500' : 'text-red-500'}>{ru.status}</div>
                   )}
+                </td>
+                <td className="py-4 px-6 text-center">
+                  <input
+                    type="checkbox"
+                    checked={ru.isPresent}
+                    className="bg-gray-100 border-gray-300 text-sky-400 focus:ring-sky-200 rounded"
+                    onChange={(e) => togglePresence(ru.userId!, e.target.checked)}
+                  />
                 </td>
               </tr>
             ))}
